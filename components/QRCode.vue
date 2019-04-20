03ca5492d1bbf8c90df141071b4aaafe0ed818b3
MacRat <m@crat.jp>
2019-04-21 03:17:01 +0900

    Fixed bug that didn't updated QR by option settings

--- components/QRCode.vue
+++ components/QRCode.vue
@@ -1,57 +1,41 @@
 <style scoped>
 div > div {
-	display: flex;
-	justify-content: center;
-	align-items: center;
 	width: 100%;
 	height: 100%;
-}
-i {
-	display: block;
-	font-size: 36px;
 }
 </style>
 
 <template>
 	<div :style="{width: size + 'px', height: size + 'px'}">
-		<img :width=size :height=size :src=url v-if=url />
-		<div v-else-if=empty :style="{backgroundColor: mergedOptions.color.light}" />
-		<div v-else :style="{backgroundColor: mergedOptions.color.light}">
-			<i class="el-icon-loading" :style="{color: mergedOptions.color.dark}" />
-		</div>
+		<div v-if=empty :style="{backgroundColor: mergedOptions.color.light}" />
+		<img v-else :width=size :height=size :src=url />
 	</div>
 </template>
 
 <script>
-import Vue from 'vue';
 import QRCode from 'qrcode';
 
 
-async function tick() {
-	await new Promise((resolve, reject) => Vue.nextTick(resolve));
-}
-
-
 export default {
-    props: {
-        size: {
-            type: Number,
-            default: 200,
-        },
-        data: [String, Number],
-        fallback: {
-            type: String,
-            default: undefined,
-        },
-        options: {
-            type: Object,
-            default() {
-                return {};
-            },
-        },
-        foreground: String,
-        background: String,
-    },
+	props: {
+		size: {
+			type: Number,
+			default: 200,
+		},
+		data: [String, Number],
+		fallback: {
+			type: String,
+			default: undefined,
+		},
+		options: {
+			type: Object,
+			default() {
+				return {};
+			},
+		},
+		foreground: String,
+		background: String,
+	},
 
 	computed: {
 		mergedOptions() {
@@ -61,37 +45,17 @@ export default {
 			op.color.dark = this.foreground || op.color.dark;
 			return op;
 		},
-	},
-
-	data() {
-		return {
-			url: this.fallback,
-			empty: false,
-		};
-	},
-
-	methods: {
-        async updateURL() {
-            if (!this.data) {
-				this.url = this.fallback;
-				if (!this.fallback) this.empty = true;
-				return;
-			}
-
-			this.url = null;
-			this.empty = false;
-			await tick();
-
-            this.url = await QRCode.toDataURL(this.data, this.mergedOptions);
-        },
-    },
-	watch: {
-		async data() {
-			await this.updateURL();
+		empty() {
+			return !this.data && !this.fallback;
 		},
 	},
-	mounted() {
-		tick().then(() => this.updateURL());
+
+	asyncComputed: {
+		async url() {
+			if (!this.data) return this.fallback;
+
+			return await QRCode.toDataURL(this.data, this.mergedOptions);
+		},
 	},
 };
 </script>
