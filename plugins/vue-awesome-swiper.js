572d3e6d6ae1b0ee80987bed9ea0b68603ac805d
MacRat <m@crat.jp>
2020-06-13 21:36:35 +0900

    Updated dependencies

--- plugins/vue-awesome-swiper.js
+++ plugins/vue-awesome-swiper.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
-import VueAwesomeSwiper from 'vue-awesome-swiper/dist/ssr';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 
-import 'swiper/dist/css/swiper.css';
+import 'swiper/css/swiper.css';
 
 
 Vue.use(VueAwesomeSwiper);
